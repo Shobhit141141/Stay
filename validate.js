7fd0b4a56019b1e40eb6d0bd0b1eb9f2d3deea28
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2023-10-07 16:42:35 +0530

    Update validate.js

--- validate.js
+++ validate.js
@@ -56,7 +56,7 @@ function validateform(event){
 }
 function passcheck(){
     
-    if(passwordd.value.length==0 || !passwordd.match(/^.{8,}$/;)){
+    if(passwordd.value.length==0 || !passwordd.match(/^.{8,}$/)){
         passwordd.style.borderBottomColor="red";
         return false;
     }
